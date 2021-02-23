22bb348e40907155d3d8692dc46a7f5679ad21a2
ytian <944461466@qq.com>
2021-02-22 19:06:34 -0500

    More hints when downloading

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ pretrain_name = 'wiki-news-300d-1M.vec'
 if not path.exists(pretrain_name):
     zipurl = 'https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip'
     with urlopen(zipurl) as zipresp, NamedTemporaryFile() as tfile:
+        print('Downloading the pretrained embeddings...')
         tfile.write(zipresp.read())
         tfile.seek(0)
         unpack_archive(tfile.name, '.', format = 'zip')
